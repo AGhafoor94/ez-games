c09e04f3201b7d0cf3d8906775a731e35e9ca3b7
andrewroar <59775822+andrewroar@users.noreply.github.com>
2020-07-18 11:43:31 +0100

    constructing post method for adding favourite game

--- src/routes/html-routes.js
+++ src/routes/html-routes.js
@@ -4,6 +4,12 @@ const isAuthenticated = require("../middleware/isAuthenticated");
 
 const router = express.Router();
 
+const axios = require("axios");
+
+const user = require("../models/user");
+
+const game = require("../models/games");
+
 router.get("/", (req, res) => {
   if (req.user) {
     res.redirect("/dashboard");
@@ -18,20 +24,40 @@ router.get("/login", (req, res) => {
   res.render("login");
 });
 
-router.get("/signup", (req, res) => {
-  if (req.user) {
-    res.redirect("/dashboard");
-  }
-  res.render("signup");
-});
+router.get("/signup", (req, res) => {});
 
 router.get("/logout", (req, res) => {
   req.logout();
   res.redirect("/");
 });
 
-router.get("/dashboard", isAuthenticated, (req, res) => {
+router.get("/dashboard", isAuthenticated, async (req, res) => {
   res.render("dashboard", { email: req.user.email });
+});
+
+router.post("/test", async (req, res) => {
+  const name = "Fallout";
+  const settings = {
+    url: "https://api-v3.igdb.com/games",
+    method: "POST",
+    timeout: 0,
+    headers: {
+      Accept: "application/json",
+      "user-key": "878032e38a732e4781301afaf69add0a",
+      "Content-Type": "text/plain",
+      Cookie: "__cfduid=d3d5c0fcf3ee861790c0d0f11a789c0ff1594471408",
+    },
+    data: `fields *;where name = "${name}";`,
+  };
+
+  const response = await axios(settings);
+  console.log(response.data);
+
+  //res.render("favourite", { game: response.data });
+  const cb = (result) => {
+    res.send(result);
+  };
+  game.create(req.body.id, req.body.name, req.body.genres, 2, cb);
 });
 
 module.exports = router;
